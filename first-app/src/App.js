55dab6f99b55f19f4237cca0213cb5a9fdd91d3d
Usman Fayyaz <usman.fayyaz70@hotmail.com>
2022-05-23 07:54:17 -0700

    Buttons Added

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Components/Header'
 function App() {
-
+  const [count, setCount] = useState(0);
   return (
     <div className='App'>
       <Header/>
+      <h1>Count is: {count}</h1>
+      <button onClick={()=>{setCount(count+10)}}>+</button>
+      <button onClick={()=>{setCount(count-1)}}>-</button>
     </div>
   )
 }
